92dae4ac90591e104f1aac9d81a987f3138add2e
tana1024 <keiskbusiness@gmail.com>
2020-12-14 09:11:32 +0000

    CORS modify

--- counter-with-create-react-app-ts/frontend/src/components/counter.tsx
+++ counter-with-create-react-app-ts/frontend/src/components/counter.tsx
@@ -3,11 +3,10 @@ import React from 'react';
 import {view} from '@layr/react-integration';
 import type {Counter as BackendCounter} from '../../../backend/src/components/counter';
 
-//var os = require('os');
+var os = require('os');
 
 export const getCounter = async () => {
-//  const client = new ComponentHTTPClient('https://3001-' + os.hostname().substring(5));
-  const client = new ComponentHTTPClient('https://3001-b91f93fa-5b6f-4e43-a2d7-c23462205009.ws-us03.gitpod.io/');
+  const client = new ComponentHTTPClient('https://3001-' + os.hostname().substring(5));
 
   const BackendCounterProxy = (await client.getComponent()) as typeof BackendCounter;
 
